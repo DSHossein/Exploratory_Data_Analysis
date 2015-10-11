250f6039a5563728f7a3b109bce209feec1b60b0
DSHossein <dshosseinyousefi@gmail.com>
2015-10-11 21:28:41 +0300

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,19 @@
+#load the data to a data frame
+df=read.csv('./data/household_power_consumption.txt',sep = ';')
+
+#change the class of Date column to Date
+df=transform(df,Date=as.Date(Date,"%d/%m/%Y"))
+#subsetting the data based on two different Dates
+Date1<-as.Date('2007/02/01')
+Date2<-as.Date('2007/02/02')
+df <- df[df$Date >= Date1 & df$Date <= Date2,]
+
+#Adding Datetime column that easily concatanate date and time
+datetime <- paste(as.Date(df$Date), df$Time)
+df$Datetime <- as.POSIXct(datetime)
+
+#plot2
+plot(as.numeric(df$Global_active_power)~df$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png,file='plot2.png',height=480, width=480)
+dev.off()
